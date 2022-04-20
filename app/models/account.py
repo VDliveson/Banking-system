2afa5ff9ec50d81346adc10ce4a4d2b0dcd817fa
vanshaj duggal <VDliveson>
2022-04-20 12:23:57 +0530

    Added requirements

--- app/models/account.py
+++ app/models/account.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Account(models.Model):
-    acunt_number=models.CharField(max_length=15,primary_key=True)
-    ifscoc_code=models.CharField(max_length=11,default='NULL')
+    account_number=models.CharField(max_length=15,primary_key=True)
+    ifsc_code=models.CharField(max_length=11,default='NULL')
     balance=models.DecimalField(max_digits=20,decimal_places=2)
     customer_id=models.ForeignKey('Customer',on_delete=models.CASCADE)
     account_type=models.ForeignKey('AccountType',on_delete=models.CASCADE)
