6efc88ae5482e72973b55fc41c99aa948ecf6279
vanshaj duggal <VDliveson>
2022-05-11 14:17:40 +0530

    Updated dashboard and edit info page

--- app/views/updater.py
+++ app/views/updater.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from app.models import Customer,Account,Transaction
 from .forms import MyForm
+from django.contrib.auth.hashers import make_password, check_password
 
 class UpdaterView(View):
     def get(self,request):
@@ -13,18 +14,21 @@ class UpdaterView(View):
         customer=Customer.get_Customer_by_id(request.session.get('customer'))
         first_name=request.POST['first_name']
         address=request.POST['address']
-        gender=request.POST.get('customer_gender', None)
+        gender=request.POST.get('customer_gender', customer.gender)
         last_name=request.POST['last_name']
         email=request.POST['email']
         mobile=request.POST['mobile']
         date_of_birth=request.POST['date_of_birth']
-        print(gender)
+        password=request.POST.get('password')
+        check_password=request.POST.get('check_password')
         form=MyForm()
         update=False
+        passw=False
         
         if(gender!=customer.gender):
             customer.gender=gender
             update=True
+
 
         if(first_name!=''):
             customer.first_name=first_name
@@ -41,15 +45,21 @@ class UpdaterView(View):
         if(address!=''):
             customer.address=address
             update=True
-        
         if(date_of_birth!=''):
             customer.date_of_birth=date_of_birth
             update=True
+        if(password!=''):
+            if(check_password==password):
+                update=True
+                customer.password=make_password(password)
+                messages.success(request, 'Password changed successfully')
+            else:
+                messages.error(request, 'Password and confirm password fields do not match')
         
-        customer.register()
         if(not update):
             messages.error(request,'No data received')
         else:
+            customer.register()
             messages.success(request,'Information updated successfully')
         return render(request, 'updater.html',{'customer':customer,'form':form})
             
