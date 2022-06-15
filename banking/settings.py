a15658dcec4b5ff996b7b079b3f17c2e18e243dc
vanshaj duggal <VDliveson>
2022-06-15 12:48:19 +0530

    Edited settings

--- banking/settings.py
+++ banking/settings.py
@@ -29,7 +29,7 @@ STATIC_DIR=os.path.join(BASE_DIR,"static")
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
