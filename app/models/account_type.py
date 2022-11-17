82b29c21a0bed1038a235922e05f68777cb49893
VDliveson <vanshajduggal1234@gmail.com>
2022-11-17 21:03:42 +0530

    Minor fixes in generator

--- app/models/account_type.py
+++ app/models/account_type.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class AccountType(models.Model):
-    account_type = models.CharField(max_length=20,primary_key=True)
+    account_type = models.CharField(max_length=20,unique=True)
     
     def __str__(self):
         return self.account_type
