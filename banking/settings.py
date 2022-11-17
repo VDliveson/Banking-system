702274a4de3bf78756b36b11f248300fba7e0c71
VDliveson <vanshajduggal1234@gmail.com>
2022-11-18 00:21:11 +0530

    Edited setting.py

--- banking/settings.py
+++ banking/settings.py
@@ -24,7 +24,7 @@ STATIC_DIR=os.path.join(BASE_DIR,"static")
 
 
 env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
