793a58ff66d3442f749cedd68c3e39c041e03124
VDliveson <vanshajduggal1234@gmail.com>
2022-11-17 19:31:01 +0530

    Added staticfiles

--- banking/settings.py
+++ banking/settings.py
@@ -15,15 +15,19 @@ from pathlib import Path
 import os
 import environ
 
-env = environ.Env()
-environ.Env.read_env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR=os.path.join(BASE_DIR,"static")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR=os.path.join(BASE_DIR,"static")
+
+
+env = environ.Env()
+environ.Env.read_env()
+env_file=str(BASE_DIR) + '/.env'
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'crispy_forms',
+    'django.conf',
     'import_export',
 ]
 
